0728b4ee7fff406dd01d50b39218a81b6081f674
Feng Shang <fshang@apple.com>
2023-11-29 16:08:47 -0800

    sha1

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -7,7 +7,14 @@ export async function GET(request: Request) {
   const ticketRes = await fetch('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+token.access_token+'&type=jsapi')
   const ticket = await ticketRes.json()
 
-  console.log(ticket)
+  const t = ticket.ticket
+  let o = {
+    appId: appId,
+    nonceStr: 'hello word',
+    timestamp: new Date().getTime() / 1000 + '',
+    signature: ''
+  }
+  //o.signature =
   return Response.json({ ticket })
 
 }
