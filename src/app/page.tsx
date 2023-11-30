e91418f75ab140218ebe08fb533e1edf3e4754c2
Feng Shang <fshang@apple.com>
2023-11-30 14:18:17 -0800

    front 7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
           timestamp: j.timestamp,
           signature: j.signature,
           jsApiList: [
+            // @ts-ignore
             'checkJSApi',
             'downloadImage'
           ]
